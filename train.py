e686d131c9f4802c92365fdb776b66781f7d41d9
khashashin <khashashin@users.noreply.github.com>
2023-01-23 16:30:50 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,12 +1,18 @@
 import tensorflow as tf
 
-# Load the T5 model
-model = tf.hub.load("tensorflow/t5", "model", version="0.5.1")
+class TranslateModel:
+    def __init__(self):
+        self.model = tf.hub.load("tensorflow/t5", "model", version="0.5.1")
 
-# Prepare the dataset
-dataset = tf.data.TextLineDataset(["data_ce_ru.txt"])
-dataset = dataset.map(lambda x: ({"inputs": x}, {"targets": x}))
-dataset = dataset.shuffle(1024).batch(32).prefetch(tf.data.AUTOTUNE)
+    def train(self):
+        # Prepare the dataset
+        dataset = tf.data.TextLineDataset(["data_ce_ru.txt"])
+        dataset = dataset.map(lambda x: ({"inputs": x}, {"targets": x}))
+        dataset = dataset.shuffle(1024).batch(32).prefetch(tf.data.AUTOTUNE)
 
-# Fine-tune the model on the dataset
-model.fine_tune(dataset, steps_per_epoch=1000, save_best_model_at="translate_chechen_to_russian")
+        # Fine-tune the model on the dataset
+        self.model.fine_tune(dataset, steps_per_epoch=1000, save_best_model_at="translate_chechen_to_russian")
+
+if __name__ == "__main__":
+    model = TranslateModel()
+    model.train()
